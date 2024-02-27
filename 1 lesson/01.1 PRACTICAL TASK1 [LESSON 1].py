3690a85831770a60176894d21abd4841d45b4278
sctpilg1337 <vovayevst@gmail.com>
2024-02-27 15:26:12 +0200

    All homework

---
+++ 1 lesson/01.1 PRACTICAL TASK1 [LESSON 1].py
@@ -0,0 +1,14 @@
+name = input ("What is your name?")
+age = input ("How old are you?")
+city = input ("Where do you live?")
+
+print ("Hello," , name)
+print ("Your age is:" , age)
+print ("You live in" , city)
+
+
+#Перше написав так:  
+#print ("Hello," + name)
+#print ("Your age is:" + age)
+#print ("You live in" + city)
+#Aле мені не подобалась відповідь без пробілу, тому змінив "+" на "," 
---
+++ 1 lesson/01.2 PRACTICAL TASK2 [LESSON 1] another.py
@@ -0,0 +1,23 @@
+a = int(input("Write first number: "))
+b = int(input("Write second number: "))
+
+print("A + B =", a + b)
+print("A - B =", a - b)
+print("A * B =", a * b)
+print("A / B =", a / b)
+print("A ** B =", a ** b)
+print("A // B =", a // b)
+print("A % B =", a % b)
+
+
+#Я не зрозумів докінця завдання, тому зробив дві версії
+#a = int(3)
+#b = int(5)
+
+#print (a + b)
+#print (a - b)
+#print (a * b)
+#print (a / b)
+#print (a ** b)
+#print (a // b)
+#print (a % b)
